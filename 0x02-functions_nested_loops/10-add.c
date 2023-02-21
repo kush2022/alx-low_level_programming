3b4661bbe0c6cd7997abc5fd62ad5556b9424485
Your Name <you@example.com>
2023-02-21 09:43:08 -0800

    a +b'

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,6 +2,9 @@
 /**
  * print_to_98 - Prints all natural numbers
  * from n to 98.
+ *
+ * @n: input number.
+ *
  * Return: no return.
  */
 void print_to_98(int n)
@@ -22,4 +25,3 @@ printf("%d, ", n);
 }
 printf("%d\n", n);
 }
-
