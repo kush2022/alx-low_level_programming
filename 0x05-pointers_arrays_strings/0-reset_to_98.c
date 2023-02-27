23d845186d37f93516cbf5e94521d5387bfacc2e
Your Name <you@example.com>
2023-02-27 09:56:01 -0800

    98 Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,9 @@
-#include "main.h"
+#include <stdio.h>
 /**
  * reset_to_98 - a function that takes a pointer to an int as parameter
  * and updates the value it points to to 98
  * @n: input
- * Return: n
+ * Return: nothing
  */
 void reset_to_98(int *n)
 {
